e4e4944b6be51ae379502dda2ea89c7bfb496ed2
kalirobi <kali_robi@hotmail.com>
2025-03-31 18:25:33 +0300

    additional views

--- views_by_airflow/tickets_by_severity.py
+++ views_by_airflow/tickets_by_severity.py
@@ -1,4 +1,11 @@
+import pandas as pd
+from datetime import datetime, timedelta
+from airflow.operators.python import PythonOperator
+from airflow.providers.elasticsearch.hooks.elasticsearch import ElasticsearchHook
+
 #5. Tickets by Severity
+
+
 
 #    X-axis: severity
 
